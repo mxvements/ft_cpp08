035e30fb59e3df3d5ae410338b3d024e00b40011
luciama2 <luciama2@student.42madrid.com>
2025-03-29 18:50:18 +0100

    ex00 added tests and empty protection

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: luciama2 <luciama2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 20:17:05 by luciama2          #+#    #+#             */
-/*   Updated: 2025/03/28 20:45:14 by luciama2         ###   ########.fr       */
+/*   Updated: 2025/03/29 14:11:10 by luciama2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,8 @@ class NotFoundException : public std::exception {
 template<typename T>
 typename T::iterator easyfind(T &container, int nbr)
 {
+	if (container.empty())
+		throw NotFoundException();
 	typename T::iterator iter = std::find(container.begin(), container.end(), nbr);
 	if (iter == container.end())
 		throw NotFoundException();
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,12 +6,13 @@
 /*   By: luciama2 <luciama2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 20:17:11 by luciama2          #+#    #+#             */
-/*   Updated: 2025/03/28 20:48:26 by luciama2         ###   ########.fr       */
+/*   Updated: 2025/03/29 15:27:11 by luciama2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
 #include <list>
+#include <vector>
 
 /* ************************************************************************** */
 
@@ -24,16 +25,28 @@ const char *NotFoundException::what() const throw()
 
 int main(void)
 {
-	std::list<int> nbrs;
-	nbrs.push_back(1);
-	nbrs.push_back(2);
-	nbrs.push_back(3);
-	nbrs.push_back(5);
+	std::list<int> l;
+	l.push_back(1);
+	l.push_back(2);
+	l.push_back(3);
+	l.push_back(5);
+	std::vector<int> v;
+	v.push_back(2);
+	v.push_back(5);
 	try {
-		std::list<int>::iterator i = easyfind< std::list<int> >(nbrs, 2);
-		std::cout << "Found : " << *i << std::endl;
-		std::list<int>::iterator ii = easyfind< std::list<int> >(nbrs, 4);
-		std::cout << "Not found : " << *ii << std::endl;
+		std::list<int>::iterator list_i = easyfind< std::list<int> >(l, 2);
+		std::cout << "Found in list : " << *list_i << std::endl;
+		std::list<int>::iterator list_ii = easyfind< std::list<int> >(l, 4);
+		std::cout << "Not found in list: " << *list_ii << std::endl;
+	} catch (const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+		try {
+		std::vector<int>::iterator vector_i = easyfind< std::vector<int> >(v, 2);
+		std::cout << "Found in vector : " << *vector_i << std::endl;
+		std::vector<int>::iterator vector_ii = easyfind< std::vector<int> >(v, 4);
+		std::cout << "Not found in vector : " << *vector_ii << std::endl;
 	} catch (const std::exception &e)
 	{
 		std::cerr << e.what() << std::endl;
